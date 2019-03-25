3dd0101b07473b87fe66a7f9f73da1407f2111d4
alexsinfarosa <as898@cornell.edu>
2019-03-25 09:48:44 -0400

    Updates

---
+++ precache-manifest.2a3b8c9a572f95b695b3f08cf97989e1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff10dc2a7c199ceac732",
+    "url": "/lawn-irrigation-tool/static/css/main.3c687b3c.chunk.css"
+  },
+  {
+    "revision": "ff10dc2a7c199ceac732",
+    "url": "/lawn-irrigation-tool/static/js/main.f2867d29.chunk.js"
+  },
+  {
+    "revision": "dbbacec3d2473736c03b",
+    "url": "/lawn-irrigation-tool/static/js/runtime~main.f93e43b6.js"
+  },
+  {
+    "revision": "736395f7874b0da9c6d7",
+    "url": "/lawn-irrigation-tool/static/css/2.c3f8eff6.chunk.css"
+  },
+  {
+    "revision": "736395f7874b0da9c6d7",
+    "url": "/lawn-irrigation-tool/static/js/2.beb7c93f.chunk.js"
+  },
+  {
+    "revision": "495d50893d3304e4ce99e202c7eb09c3",
+    "url": "/lawn-irrigation-tool/static/media/spraySprinkler.495d5089.png"
+  },
+  {
+    "revision": "7380c4ae4536e25be61f8748c8e5de7d",
+    "url": "/lawn-irrigation-tool/static/media/singleStreamRotorSprinkler.7380c4ae.png"
+  },
+  {
+    "revision": "9644606a51f20fd71457837dbb0e1718",
+    "url": "/lawn-irrigation-tool/static/media/multipleStreamRotorSprinkler.9644606a.png"
+  },
+  {
+    "revision": "fb0fd3efb9c8a3033add9733bdc17a6c",
+    "url": "/lawn-irrigation-tool/static/media/moveableSprinkler.fb0fd3ef.png"
+  },
+  {
+    "revision": "b971fa20afae7536dd6bf096c8927fc0",
+    "url": "/lawn-irrigation-tool/static/media/nrcc-logo.b971fa20.svg"
+  },
+  {
+    "revision": "e5b0271bf2284ad0bbd8634e3c4b6fd7",
+    "url": "/lawn-irrigation-tool/static/media/AW-NEW YORK_PMS.e5b0271b.svg"
+  },
+  {
+    "revision": "4be86e609a1c1b8979d0de73e0c78688",
+    "url": "/lawn-irrigation-tool/index.html"
+  }
+];
